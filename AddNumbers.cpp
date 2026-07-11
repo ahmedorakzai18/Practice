3e1f63da6b3ce083667c72bb5d92cec3f6697e35
Ahmad Orakzai <ahmedorakzai18@gmail.com>
2026-07-11 21:46:17 +0500

    AddNumbers.cpp

---
+++ AddNumbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+  int a,b;
+  cout << "Enter Number 1: "; 
+  cin >> a; 
+  cout << "Enter Number 2: ";
+  cin >> b; 
+
+  cout << "Sum is: " << a+b ; 
+return 0; 
+}
