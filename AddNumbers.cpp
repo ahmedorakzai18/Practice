b9da2c4aac6b81aa15df6cd1aad1f1d4bb747460
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-07-11 10:23:04 +0500

    GoodNight

--- AddNumbers.cpp
+++ AddNumbers.cpp
@@ -2,12 +2,15 @@
 using namespace std; 
 int main()
 {
-  int a,b;
+  float a,b;
   cout << "Enter Number 1: "; 
   cin >> a; 
   cout << "Enter Number 2: ";
   cin >> b; 
 
   cout << "Sum is: " << a+b ; 
+  cout << "\nSubtraction is: "<< a-b; 
+  cout << "\nMultiplication is: " << a*b; 
+  cout << "\nDivision is: " << a/b; 
 return 0; 
 }
